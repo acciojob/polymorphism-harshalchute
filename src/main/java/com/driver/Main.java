b051703c6620272c94a4d9eb7df8a9090f211ca4
Harshalchute <harshalchute1@gmail.com>
2024-02-09 23:44:00 +0530

    Assignment to understand polymorphism public issue fixed

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -11,7 +11,7 @@ public class Main {
         double ans2 = p.product(1.0,2.0);
     }
 
-    static class Product {
+    public static class Product {
         public int product(int x, int y) {
             return x * y;
         }
